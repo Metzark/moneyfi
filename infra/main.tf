65d47f1646302f5beba61bc0b646dd655faa779c
Jonas Metzger <Jonas.A.Metzger-1@ou.edu>
2025-03-22 15:04:30 -0500

    Testing terraform action

--- infra/main.tf
+++ infra/main.tf
@@ -7,7 +7,7 @@ resource "aws_lightsail_instance" "moneyfi" {
   availability_zone = "us-east-2a"
   blueprint_id      = "ubuntu_20_04"
   bundle_id         = "nano_2_0"
-  key_pair_name     = "moneyfi-us-east-2"
+  key_pair_name     = "moneyfi"
 
   tags = {
     Name = "moneyfi"
