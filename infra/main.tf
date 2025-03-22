3b6c6416fa2708fa7c01c4c5e697265cf03dc596
Jonas Metzger <Jonas.A.Metzger-1@ou.edu>
2025-03-22 15:20:42 -0500

    Testing terraform action

--- infra/main.tf
+++ infra/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_lightsail_instance" "moneyfi-nextjs" {
-  name              = "moneyfi-1`}"
+  name              = "moneyfi-1"
   availability_zone = "us-east-2a"
   blueprint_id      = "ubuntu_20_04"
   bundle_id         = "nano_2_0"
