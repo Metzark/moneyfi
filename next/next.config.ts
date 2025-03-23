688f3f27a7ebd34b5dbc79828d29157cc4717f99
Jonas Metzger <Jonas.A.Metzger-1@ou.edu>
2025-03-22 21:56:59 -0500

    Downgrading to next 18

--- next/next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  experimental: {
-    allowedDevOrigins: ["https://moneyfi.metzark.com"],
-  },
-};
-
-export default nextConfig;
