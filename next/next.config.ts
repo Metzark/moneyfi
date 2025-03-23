4a4294ce7d2e0b0fd0bb24452675e29f91961b7e
Jonas Metzger <Jonas.A.Metzger-1@ou.edu>
2025-03-22 21:26:50 -0500

    Adding dev cors

--- next/next.config.ts
+++ next/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    allowedDevOrigins: ["https://moneyfi.metzark.com"],
+  },
 };
 
 export default nextConfig;
